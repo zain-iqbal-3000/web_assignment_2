c61a4c20636bde7c22b155e5201772e15a72ba38
zain-iqbal-3000 <zainiqbal.35201@gmail.com>
2025-05-10 21:28:42 +0500

    made the app serverless

--- Backend/server.js
+++ Backend/server.js
@@ -3,7 +3,7 @@ const dotenv = require('dotenv');
 const cors = require('cors');
 const connectDB = require('./config/db');
 const taskRoutes = require('./routes/taskRoutes');
-
+const serverless = require('serverless-http');
 
 // Load env vars
 dotenv.config();
@@ -15,10 +15,10 @@ const app = express();
 
 // Middleware
 const corsOptions = {
-    origin: ['http://localhost:3000', 'http://127.0.0.1:3000', 'http://127.0.0.1.5500 '], 
-    optionsSuccessStatus: 200
-  };
-  app.use(cors(corsOptions));
+  origin: ['http://localhost:3000', 'http://127.0.0.1:3000', 'http://127.0.0.1:5500'],
+  optionsSuccessStatus: 200
+};
+app.use(cors(corsOptions));
 app.use(express.json());
 
 // Routes
@@ -27,8 +27,5 @@ app.use('/api/tasks', taskRoutes);
 // Serve static files from frontend folder
 app.use(express.static('frontend'));
 
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+// Export the app as a serverless handler
+module.exports.handler = serverless(app);
